cf4295b2755d4d76541486f999fc49505041233c
Nopparat <nopparat.mueangkaew@gmail.com>
2024-09-06 16:59:53 +0700

    Add API DR19

--- app/api/flex1/route.js
+++ app/api/flex1/route.js
@@ -4,8 +4,23 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   // Create a simple Flex Message JSON response
   const flexMessage = {
-    type: "flex",
-    message: "Flex Message 1"
+    stockName1: "NINTENDO19",
+    stockNew1: "New",
+
+    stockName2: "SIA19",
+    stockNew2: "",
+
+    stockName2: "STEG19",
+    stockNew2: "",
+
+    stockName2: "VENTURE19",
+    stockNew2: "",
+
+    stockName2: "SMFG19",
+    stockNew2: "",
+
+    stockName2: "GOLD19",
+    stockNew2: ""
   };
 
   // Return a JSON response
